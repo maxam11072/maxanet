e32cf833e39d80b57d066fde220875611a3e81f1
maxam11072 <81041435+maxam11072@users.noreply.github.com>
2021-03-29 03:47:42 +0500

    cpp code c++ language
    code written

---
+++ Source1.cpp
@@ -0,0 +1,47 @@
+//#include <iostream>
+//#include <iomanip> 
+//
+//using namespace std;
+//
+//int main() {
+//
+//	cout << setw(50) << setfill('-') << "-" << endl;
+//
+//	//FIRST ROW CODE 
+//	cout <<"|" << setw(6) << setfill(' ') << "NAME" << setw(3) << setfill(' ') << "|";        
+//	cout << setw(13) << setfill(' ') << "ENROLLMENTS" << setw(3) << setfill(' ') << "|";       
+//	cout << setw(5) << setfill(' ') << "AGE" << setw(3) << setfill(' ') << "|";                
+//	cout << setw(13) << setfill(' ') << "TOTAL MARKS" << setw(3) << setfill(' ') << "|" << endl;
+//
+//	cout << setw(50) << setfill('-') << "-" << endl;
+//
+//	//SECOND ROW CODE
+//	cout << "|" << setw(7) << setfill(' ') << "MAYSAM" << setw(2) << setfill(' ') << "|";
+//	cout << setw(14) << setfill(' ') << "03-134211-017" << setw(2) << setfill(' ') << "|";
+//	cout << setw(5) << setfill(' ') << "19Y" << setw(3) << setfill(' ') << "|";
+//	cout << setw(11) << setfill(' ') << "075/100" << setw(5) << setfill(' ') << "|" << endl;
+//	
+//	//THIRD ROW CODE
+//	cout << "|" << setw(7) << setfill(' ') << "HASEEB" << setw(2) << setfill(' ') << "|";
+//	cout << setw(14) << setfill(' ') << "03-134211-009" << setw(2) << setfill(' ') << "|";
+//	cout << setw(5) << setfill(' ') << "18Y" << setw(3) << setfill(' ') << "|";
+//	cout << setw(11) << setfill(' ') << "072/100" << setw(5) << setfill(' ') << "|" << endl;
+//
+//	//FOURTH ROW CODE
+//	cout << "|" << setw(7) << setfill(' ') << "HASSAN" << setw(2) << setfill(' ') << "|";
+//	cout << setw(14) << setfill(' ') << "03-134211-012" << setw(2) << setfill(' ') << "|";
+//	cout << setw(5) << setfill(' ') << "19Y" << setw(3) << setfill(' ') << "|";
+//	cout << setw(11) << setfill(' ') << "085/100" << setw(5) << setfill(' ') << "|" << endl;
+//
+//	//FIFTH ROW CODE
+//	cout << "|" << setw(7) << setfill(' ') << "ASGHAR" << setw(2) << setfill(' ') << "|";
+//	cout << setw(14) << setfill(' ') << "03-134211-014" << setw(2) << setfill(' ') << "|";
+//	cout << setw(5) << setfill(' ') << "17Y" << setw(3) << setfill(' ') << "|";
+//	cout << setw(11) << setfill(' ') << "081/100" << setw(5) << setfill(' ') << "|" << endl;
+//
+//	cout << setw(50) << setfill('-') << "-" << endl;
+//
+//	return(0);
+//	system("pause");
+
+
